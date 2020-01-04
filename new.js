3c0f5bd3563fc146bc28f6a57ab67542319bb3d7
yinze00 <3170105970@zju.edu.cn>
2020-01-04 10:04:43 +0800

    UPDATE

--- new.js
+++ new.js
@@ -5,8 +5,8 @@
 
 function main() {
   var image = new Image();
-  requestCORSIfNotSameOrigin(image, "c4061c796badc21.jpg")
-  image.src = "";
+  requestCORSIfNotSameOrigin(image, "http://n.sinaimg.cn/sinacn20123/1/w2048h1153/20190731/74de-iaqfzyv5166574.jpg")
+  image.src = "http://n.sinaimg.cn/sinacn20123/1/w2048h1153/20190731/74de-iaqfzyv5166574.jpg";
   image.onload = function () {
     render(image);
   };
@@ -95,106 +95,26 @@ function render(image) {
 
   // Define several convolution kernels
   var kernels = {
-    normal: [
-      0, 0, 0,
-      0, 1, 0,
-      0, 0, 0
-    ],
-    gaussianBlur: [
-      0.045, 0.122, 0.045,
-      0.122, 0.332, 0.122,
-      0.045, 0.122, 0.045
-    ],
-    gaussianBlur2: [
-      1, 2, 1,
-      2, 4, 2,
-      1, 2, 1
-    ],
-    gaussianBlur3: [
-      0, 1, 0,
-      1, 1, 1,
-      0, 1, 0
-    ],
-    unsharpen: [
-      -1, -1, -1,
-      -1, 9, -1,
-      -1, -1, -1
-    ],
-    sharpness: [
-      0, -1, 0,
-      -1, 5, -1,
-      0, -1, 0
-    ],
-    sharpen: [
-      -1, -1, -1,
-      -1, 16, -1,
-      -1, -1, -1
-    ],
-    edgeDetect: [
-      -0.125, -0.125, -0.125,
-      -0.125, 1, -0.125,
-      -0.125, -0.125, -0.125
-    ],
-    edgeDetect2: [
-      -1, -1, -1,
-      -1, 8, -1,
-      -1, -1, -1
-    ],
-    edgeDetect3: [
-      -5, 0, 0,
-      0, 0, 0,
-      0, 0, 5
-    ],
-    edgeDetect4: [
-      -1, -1, -1,
-      0, 0, 0,
-      1, 1, 1
-    ],
-    edgeDetect5: [
-      -1, -1, -1,
-      2, 2, 2,
-      -1, -1, -1
-    ],
-    edgeDetect6: [
-      -5, -5, -5,
-      -5, 39, -5,
-      -5, -5, -5
-    ],
-    sobelHorizontal: [
-      1, 2, 1,
-      0, 0, 0,
-      -1, -2, -1
-    ],
-    sobelVertical: [
-      1, 0, -1,
-      2, 0, -2,
-      1, 0, -1
-    ],
-    previtHorizontal: [
-      1, 1, 1,
-      0, 0, 0,
-      -1, -1, -1
-    ],
-    previtVertical: [
-      1, 0, -1,
-      1, 0, -1,
-      1, 0, -1
-    ],
-    boxBlur: [
-      0.111, 0.111, 0.111,
-      0.111, 0.111, 0.111,
-      0.111, 0.111, 0.111
-    ],
-    triangleBlur: [
-      0.0625, 0.125, 0.0625,
-      0.125, 0.25, 0.125,
-      0.0625, 0.125, 0.0625
-    ],
-    emboss: [
-      -2, -1, 0,
-      -1, 1, 1,
-      0, 1, 2
-    ]
+    normal: [0, 0, 0, 0, 1, 0, 0, 0, 0],
+    gaussianBlur: [0.045, 0.122, 0.045,0.122, 0.332, 0.122,0.045, 0.122, 0.045],
+    gaussianBlur2: [1, 2, 1,2, 4, 2,1, 2, 1 ],
+    gaussianBlur3: [0, 1, 0,1, 1, 1,0, 1, 0],
+    unsharpen: [ -1, -1, -1, -1, 9, -1,-1, -1, -1],
+    sharpness: [ 0, -1, 0,-1, 5, -1,0, -1, 0],
+    sharpen: [ -1, -1, -1, -1, 16, -1, -1, -1, -1 ],
+    edgeDetect: [ -0.125, -0.125, -0.125,-0.125, 1, -0.125,-0.125, -0.125, -0.125],
+    edgeDetect2: [ -1, -1, -1,-1, 8, -1,-1, -1, -1],
+    edgeDetect3: [-5, 0, 0,0, 0, 0,0, 0, 5 ],
+    edgeDetect4: [ -1, -1, -1,0, 0, 0,1, 1, 1],
+    edgeDetect5: [-1, -1, -1, 2, 2, 2,-1, -1, -1],
+    edgeDetect6: [ -5, -5, -5,-5, 39, -5,-5, -5, -5],
+    sobelHorizontal: [1, 2, 1,0, 0, 0,-1, -2, -1],
+    sobelVertical: [1, 0, -1,2, 0, -2, 1, 0, -1 ],
+    previtHorizontal: [  1, 1, 1,  0, 0, 0,  -1, -1, -1 ],
+    previtVertical: [   1, 0, -1,   1, 0, -1,   1, 0, -1  ],
+    boxBlur: [ 0.111, 0.111, 0.111, 0.111, 0.111, 0.111,  0.111, 0.111, 0.111 ],
+    triangleBlur: [0.0625, 0.125, 0.0625, 0.125, 0.25, 0.125,  0.0625, 0.125, 0.0625 ],
+    emboss: [ -2, -1, 0,  -1, 1, 1,  0, 1, 2  ]
   };
 
   var effects = [
@@ -212,7 +132,7 @@ function render(image) {
     { name: "unsharpen", },
     { name: "emboss", on: true },
     { name: "edgeDetect", },
-    { name: "edgeDetect", },
+    { name: "edgeDetect", on: true },
     { name: "edgeDetect3", },
     { name: "edgeDetect3", },
   ];
